70a65dd45820486b20f4312f144ebf47e633ae8f
Shun Nakano <nakano_syun@waku-2.com>
2020-11-16 11:34:12 +0900

    todo作成時にuserIdの紐付け処理追加

--- app/todos/mutations/createTodo.ts
+++ app/todos/mutations/createTodo.ts
@@ -5,6 +5,12 @@ type CreateTodoInput = Pick<TodoCreateArgs, "data">
 export default async function createTodo({ data }: CreateTodoInput, ctx: Ctx) {
   ctx.session.authorize()
 
+  // sessionからuserIdの取得
+  const id = ctx.session.userId
+
+  // todoにuserIdを紐付ける
+  data.user = { connect: { id } }
+
   const todo = await db.todo.create({ data })
 
   return todo
