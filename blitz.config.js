ae8ab3579d7160a95ee972737cb75ce49ba6fe2c
Shun Nakano <nakano_syun@waku-2.com>
2020-11-17 09:17:28 +0900

    sassOption追加

--- blitz.config.js
+++ blitz.config.js
@@ -1,4 +1,5 @@
 const { sessionMiddleware, unstable_simpleRolesIsAuthorized } = require("@blitzjs/server")
+const path = require("path")
 
 module.exports = {
   middleware: [
@@ -6,6 +7,9 @@ module.exports = {
       unstable_isAuthorized: unstable_simpleRolesIsAuthorized,
     }),
   ],
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
